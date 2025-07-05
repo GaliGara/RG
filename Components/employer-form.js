60d8235bb9511b7f4ac032b49c9249daeb9f98de
GaliGara <garagali@hotmail.es>
2025-07-04 23:54:28 -0600

    render employer-form component

--- Components/employer-form.js
+++ Components/employer-form.js
@@ -20,7 +20,95 @@ export class EmployerForm extends LitElement{
 
     render() {
         return html`
-        <h1>Employers</h1>
+        <div
+        id="card-sell"
+        class="fixed inset-0 bg-black/40 flex items-center justify-center z-50"
+        >
+            <div
+             class="bg-white rounded-lg shadow-xl p-6 w-full max-w-lg relative"
+             >
+                <h2 class="text-2xl font-semibold mb-4">Registro de Empleados</h2>
+                <form>
+                    <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
+
+                        <div>
+                            <label class="block text-sm font-medium mb-1">Nombres:</label>
+                            <input
+                            type="text" 
+                            name="nombre"
+                            class="border w-full border-gray-300 rounded-md"
+                            >
+
+                        </div>
+                        <div>
+                            <label class="block text-sm font-medium mb-1">Apellido Paterno:</label>
+                            <input
+                            type="text" 
+                            name="paterno"
+                            class="border w-full border-gray-300 rounded-md"
+                            >
+                        </div>
+                        <div class="col-span-1">
+                            <label class="block text-sm font-medium mb-1">Apellido Materno:</label>
+                            
+                            <input
+                            type="text" 
+                            name="materno"
+                            class="border w-full border-gray-300 rounded-md"
+                            >
+                        </div>        
+                        <div class="col-span-1">      
+                            <label class="block text-sm font-medium mb-1">Banco:</label> 
+                                <input
+                                id="ventas"
+                                type="text"
+                                class="w-full border col-span-3 border-gray-300 rounded-md"
+                                >
+
+                        </div>
+                        <div class="col-span-2">
+                            <label class="block text-sm font-medium mb-1">Numero de cuenta:</label>
+                            
+                            <input
+                            id="ventas"
+                            type="number"
+                            step="1"
+                            class="w-full border col-span-3 border-gray-300 rounded-md"
+                            >
+
+                        </div>        
+                        <div class="col-span-2">
+                            <label class="block text-sm font-medium mb-1 ">Puesto:</label>
+                            
+                                <select
+                                name="puesto"
+                                class="w-full border col-span-7 border-gray-300 rounded-md"
+                                >
+                                    <option>Vendedor</option>
+                                    <option>Gerente</option>
+                                    <option>Director</option>
+                                </select>
+                        </div>
+
+                    </div>
+
+                    <!-- Buttons -->
+                     <div class="mt-3 flex justify-end gap-2 pt-2">
+                        <button 
+                        class="px-4 py-2 rounded-md bg-gray-200 hover:bg-gray-300"
+                        type="button">
+                            Cerrar
+                        </button>
+                        <button
+                        class="px-4 py-2 rounded-md bg-blue-600 text-white hover:bg-blue-700"
+                        >
+                            Agregar
+                        </button>
+                     </div>
+
+                </form>
+            </div>
+        </div>
         `;
     }
 }
